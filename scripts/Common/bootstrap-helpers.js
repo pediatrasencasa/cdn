b569300754d4d2a4dbb70d806e4c9f5bb9845e75
Ayaz <ayazzz@users.noreply.github.com>
2025-07-19 17:00:47 +0300

    remove non-min files

--- scripts/Common/bootstrap-helpers.js
+++
@@ -1,9 +0,0 @@
-﻿$(function () {
-    $('[data-toggle="tooltip"]').tooltip(); // getbootstrap.com/javascript/#tooltips
-
-    $('[data-toggle="popover"]').popover()  // getbootstrap.com/javascript/#popovers
-
-    if (jQuery && jQuery.fn && jQuery.fn.select2 !== undefined) {
-        $.fn.select2.defaults.set("theme", "bootstrap");
-    }
-});
